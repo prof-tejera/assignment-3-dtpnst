03db77182dfb01d296a182869600f8f992e125cb
dtpnst <dtpnst@users.noreply.github.com>
2023-12-17 19:29:39 +0000

    deploy: 1de187eebd04712236d10d7c3528917dbeecd6dc

---
+++ precache-manifest.c44ce2c240eda7003e57b855a85fe118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27db7cd6c426bc225ae13d6a3a16a553",
+    "url": "/assignment-3-dtpnst/index.html"
+  },
+  {
+    "revision": "9bce3c85e3ad019c1646",
+    "url": "/assignment-3-dtpnst/static/css/main.b0711a4e.chunk.css"
+  },
+  {
+    "revision": "5f22c37b1b55cf05ca39",
+    "url": "/assignment-3-dtpnst/static/js/2.dec87477.chunk.js"
+  },
+  {
+    "revision": "9bce3c85e3ad019c1646",
+    "url": "/assignment-3-dtpnst/static/js/main.3cdf3f85.chunk.js"
+  },
+  {
+    "revision": "37b1f417fd6064b52713",
+    "url": "/assignment-3-dtpnst/static/js/runtime~main.c7827f61.js"
+  }
+]);
--- src/views/AddTimerView.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddTimer from "../components/AddTimer";
-
-const AddTimerView = () => {
-  return <AddTimer></AddTimer>
-}
-
-export default AddTimerView;
